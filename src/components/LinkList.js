208d7a99879939f825cb540aa4de77075ccfedde
Alberto <alberto.godinez90@gmail.com>
2019-04-02 22:19:32 -0500

    - added graphql query for LinkList - added createdAt type to Link - Added Link and LinkList component - fixed issue with App.js and wrapped it with ApolloProvider

---
+++ src/components/LinkList.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Link from './Link';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+class LinkList extends Component {
+  render() {
+    // the props loading, error, data provide information
+    // about the state of the network request
+    return (
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
+
+          const linksToRender = data.feed.links;
+
+          return (
+            <div>
+              {linksToRender.map(link => (
+                <Link key={link.id} link={link} />
+              ))}
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+/**
+ * Stores the query. The gql function is used to
+ * parse the plain string that contains the GraphQL Code
+ */
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`;
+
+export default LinkList;
