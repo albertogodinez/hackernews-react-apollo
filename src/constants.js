0db2787050d56e73ad2a89dfca64206ecc9676da
Alberto <alberto.godinez90@gmail.com>
2019-04-07 21:39:11 -0500

    added pagination functionality

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,5 @@ export const AUTH_TOKEN = 'auth-token';
 
 // Warning: Storing JWT's in localStorage is not a safe approach to implement
 // authentication on the frontend.
+
+export const LINKS_PER_PAGE = 5;
